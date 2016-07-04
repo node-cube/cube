e6a20b2ada2188753119b4f13c16c3086bf2efae
HQidea <hqidea@yahoo.com>
2016-07-04 16:36:21 +0800

    chore: remove useless code

--- runtime/cube.js
+++ runtime/cube.js
@@ -197,30 +197,19 @@
    * @param callback
    */
   function Cube(name, requires, callback) {
-    //var requiresLoaded = false;
-
-    // if (arguments.length === 3) {
     if (!installedModules[name]) {
       installedModules[name] = {
         exports: {},
         loaded: false,
         fired: false
       };
-      /*
-      requiresLoaded = true;
-      if (requires.length && settings.debug) {
-        console.info('Cube Info: Module ' + '\'' + requires + '\'' + ' should exist');
-      }
-      */
     }
+
     var module = installedModules[name];
     module.fn = callback;
     module.loaded = true;
     delete loading[name];
-    //if (!requiresLoaded) {
     helpers.load(requires, name);
-    //}
-    // }
   }
 
   /** version, will replace in `make release` **/
