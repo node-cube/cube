5e47d345808675cccfe685325a0fc0587b24f1c7
fishbar <zhengxinlin@gmail.com>
2019-06-04 01:56:37 +0800

    test

--- test/cli.js
+++ test/cli.js
@@ -28,10 +28,10 @@ describe('cli', function () {
     });
   });
   describe('build', function () {
-    it('should work fine', function (done) {
+    it.only('should work fine', function (done) {
       var cmd = 'node bin/cube build example';
       exec(cmd, function (err, stdout, stderr) {
-        // console.log(stdout.toString(), stderr.toString());
+        console.log(stdout.toString(), stderr.toString());
         var res = stdout.toString().split('\n');
         var info = [];
         res.forEach(function (v) {
